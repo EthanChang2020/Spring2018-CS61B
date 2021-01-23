b89c157aa3eadabe782ce187c823a254df93d762
EthanChang2020 <yuxin955.zhang@gmail.com>
2021-01-23 15:52:59 +0800

    lab3

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,11 +1,13 @@
+
 public class HorribleSteve {
     public static void main(String [] args) {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
-                break; // break exits the for loop!
+                System.out.print("i is " + i + " j is " + j);
+                break;
             }
         }
-        System.out.println("i is " + i);
     }
+
 }
